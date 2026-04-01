17978eb80405e158a9ec88e5bb623890371bab35
Jessica Hernandez <jessicahernandez@Jessicas-MacBook-Pro.local>
2026-04-01 09:57:50 -0800

    Add API integration with fetchAPI and fallback times

--- src/Main.js
+++ src/Main.js
@@ -4,19 +4,6 @@ import { initializeTimes, updateTimes } from "./utils";
 import Homepage from "./Homepage";
 import BookingPage from "./BookingPage";
 
-export function updateTimes(state, action) {
-  switch (action.type) {
-    case "UPDATE_TIMES":
-      return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-    default:
-      return state;
-  }
-}
-
-export function initializeTimes() {
-  return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-}
-
 function Main() {
   const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
 
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,24 @@
+/* global fetchAPI */
+
+const defaultTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
 export function initializeTimes() {
-  return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  try {
+    const today = new Date();
+    return fetchAPI(today);
+  } catch {
+    return defaultTimes;
+  }
 }
 
 export function updateTimes(state, action) {
   switch (action.type) {
     case "UPDATE_TIMES":
-      return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+      try {
+        return fetchAPI(new Date(action.date));
+      } catch {
+        return defaultTimes;
+      }
     default:
       return state;
   }
