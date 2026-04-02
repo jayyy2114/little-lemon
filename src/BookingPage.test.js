0fee42ddcb0cbcd26b72dd26c057e682414adc5f
Jessica Hernandez <jessicahernandez@Jessicas-MacBook-Pro.local>
2026-04-01 18:45:13 -0800

    Add validation unit test for BookingPage

--- src/BookingPage.test.js
+++ src/BookingPage.test.js
@@ -3,8 +3,37 @@ import BookingPage from "./BookingPage";
 
 test("Renders the BookingPage heading", () => {
   render(
-    <BookingPage availableTimes={["17:00", "18:00"]} dispatch={() => {}} />,
+    <BookingPage
+      availableTimes={["17:00", "18:00"]}
+      dispatch={() => {}}
+      submitForm={() => {}}
+    />,
   );
   const headingElement = screen.getByText("Reserve a Table");
   expect(headingElement).toBeInTheDocument();
+});
+
+test("date input has required attribute", () => {
+  render(
+    <BookingPage
+      availableTimes={["17:00"]}
+      dispatch={() => {}}
+      submitForm={() => {}}
+    />,
+  );
+  const dateInput = screen.getByLabelText("Choose date");
+  expect(dateInput).toHaveAttribute("required");
+});
+
+test("guests input has min and max attributes", () => {
+  render(
+    <BookingPage
+      availableTimes={["17:00"]}
+      dispatch={() => {}}
+      submitForm={() => {}}
+    />,
+  );
+  const guestsInput = screen.getByRole("spinbutton");
+  expect(guestsInput).toHaveAttribute("min", "1");
+  expect(guestsInput).toHaveAttribute("max", "10");
 });
